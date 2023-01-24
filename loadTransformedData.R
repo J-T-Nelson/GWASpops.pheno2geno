0f40ea8b73c612836bef6a19daa1921423e8f3ee
Jon Tanner Nelson <38541965+J-T-Nelson@users.noreply.github.com>
2023-01-23 19:45:10 -0500

    Helpful scripts added

---
+++ loadTransformedData.R
@@ -0,0 +1,14 @@
+# Load graphing example data
+
+load('./exampleData/transformed_data_for_graphing/airPollutionPops.rds')
+load('./exampleData/transformed_data_for_graphing/alcConsumpPops.rds')
+load('./exampleData/transformed_data_for_graphing/bCarcinomaPops.rds')
+load('./exampleData/transformed_data_for_graphing/colorectalCancerPops.rds')
+load('./exampleData/transformed_data_for_graphing/IBDPops.rds')
+load('./exampleData/transformed_data_for_graphing/IntPops.rds')
+load('./exampleData/transformed_data_for_graphing/malabsorptionSyndPops.rds')
+load('./exampleData/transformed_data_for_graphing/lungCancerPops.rds')
+load('./exampleData/transformed_data_for_graphing/neuroticismPops.rds')
+load('./exampleData/transformed_data_for_graphing/prostateCancerPops.rds')
+load('./exampleData/transformed_data_for_graphing/substanceAbusePops.rds')
+load('./exampleData/transformed_data_for_graphing/malabsorptionSyndPops.rds')
